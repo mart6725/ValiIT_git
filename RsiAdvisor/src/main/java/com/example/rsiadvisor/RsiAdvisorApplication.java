8cf2677a95918e551053c58a9756298e3783754f
Peep <abcde@gmail.com>
2021-12-06 15:42:28 +0200

    new projectrsi

---
+++ RsiAdvisor/src/main/java/com/example/rsiadvisor/RsiAdvisorApplication.java
@@ -0,0 +1,13 @@
+package com.example.rsiadvisor;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RsiAdvisorApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(RsiAdvisorApplication.class, args);
+    }
+
+}
